648f576cdb397b7ef2fc67e2c538f30175458a8a
Devin Zebertavage <60449275+dzebertavage@users.noreply.github.com>
2025-06-06 20:37:38 -0400

    Add functionality to modal window elements

--- js/script.js
+++ js/script.js
@@ -16,13 +16,70 @@ const changeDateCheckbox = document.querySelector("#changeDate");
 const newDateDiv = document.querySelector(".new-date");
 const newDate = document.querySelector("#newDate");
 const sellByDateText = document.querySelector(".sell-by-date");
-
+const weight = document.querySelector(".weight");
+const groundCoffeeBanner = document.querySelector(".ground-coffee-banner");
 const groundSliderBox = document.querySelector(".ground-select");
+const coffeePicker = document.querySelector("#coffee");
+const discardBtn = document.querySelector(".discard-btn");
+const keepBtn = document.querySelector(".keep-btn");
+const groundCoffeeText = document.querySelector(".ground-coffee-text");
 
-groundSliderBox.style.display = 'none';
+function displayRangeNum(val) {
+    groundNumPreview.textContent = val;
+}
+
 sellByDateText.textContent = sellByDate;
 
 editBtn.addEventListener('click', () => {
     editDialogBox.showModal();
+    changeDateCheckbox.checked = false;
+    coffeePicker.value = "";
+    wholeRadioBtn.checked = true;
+    groundSliderBox.style.display = "none";
+    fiveRadio.checked = true;
+    newDate.value = "";
+    newDateDiv.style.display = "none";
 });
 
+groundRadioBtn.addEventListener('change', (event) => {
+    if (event.target.checked) {
+        groundSliderBox.style.display = "grid";
+        groundNumPreview.textContent = groundSlider.value;
+    }
+});
+
+wholeRadioBtn.addEventListener('change', (event) => {
+    if (event.target.checked) {
+        groundSliderBox.style.display = "none";
+        groundSlider.value = 5;
+        groundNumPreview.textContent = groundSlider.value;
+    }
+});
+
+changeDateCheckbox.addEventListener('change', () => {
+    if (changeDateCheckbox.checked) {
+        newDateDiv.style.display = "grid";
+    } else {
+        newDateDiv.style.display = "none";
+    }
+});
+
+discardBtn.addEventListener('click', () => {
+    changeDateCheckbox.checked = false;
+    coffeePicker.value = "";
+    wholeRadioBtn.checked = true;
+    groundSliderBox.style.display = "none";
+    fiveRadio.checked = true;
+    newDate.value = "";
+    newDateDiv.style.display = "none";
+});
+
+keepBtn.addEventListener('click', () => {
+    if (groundRadioBtn.checked) {
+        groundCoffeeBanner.style.display = "grid";
+        groundCoffeeText.textContent = "Ground #" + groundSlider.value;
+    } else {
+        groundCoffeeBanner.style.display = "none";
+    }
+    
+});
